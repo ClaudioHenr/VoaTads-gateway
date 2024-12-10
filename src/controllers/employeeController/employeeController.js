05b54d2ae077919f5d2895ac7bf6394b782b2559
Vinicius Ratzke Servelo <vinicius_servelo@hotmail.com>
2024-12-09 21:10:07 -0300

    saga insert employee feita

--- src/controllers/employeeController/employeeController.js
+++ src/controllers/employeeController/employeeController.js
@@ -2,36 +2,39 @@ const axios = require('axios')
 
 // Utils
 const { sendEmail } = require('../../utils/sendEmail')
+
+const employeeServiceUrl = 'http://localhost:5001'; // URL do serviço de customer
 const sagaServiceUrl = 'http://localhost:5010'; // URL do serviço de saga
 
 async function getEmployees(req, res) {
     try {
-        const response = await axios.get('http://localhost:5001/employees')
+        const response = await axios.get(`${employeeServiceUrl}/employees`)
         const users = response.data
         if (users) {
             return res.status(200).json({
-                Employees: users
+                employees: users
             })
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Erro ao acessar o serviço de funcionarios' });
+        res.status(500).json({ message: 'Erro serviço funcionario' });
     }
 }
 
 async function getEmployee(req, res) {
-    const { id } = req.params
+    const { id } = req.params;
     try {
-        const response = await axios.get(`http://localhost:5001/employees/${id}`)
-        const user = response.data
+        const response = await axios.get(`${employeeServiceUrl}/employees/${id}`);
+        const user = response.data;
         if (user) {
             return res.status(200).json({
-                Employees: user
-            })
+                employees: user,
+                miles: user.miles
+            });
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Erro ao acessar o serviço de funcionarios' });
+        res.status(500).json({ message: 'Erro serviço funcionario' });
     }
 }
 
@@ -42,22 +45,21 @@ async function createEmployee(req, res) {
         console.log(newEmployee)
         const response = await axios.post(`${sagaServiceUrl}/employees/create`, newEmployee)
         return res.status(201).json({
-            message: "Solicitação de criação de func. enviada com sucesso"
+            message: "Solicitação de criação de funcionario enviada com sucesso"
         })
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Erro na criação de func.' });
+        res.status(500).json({ message: 'Erro na criação de funcionario' });
     }
 }
-
 
 async function updateEmployee(req, res) {
     const { id } = req.params
     const updateEmployee = req.body
     console.log("Em body: ", req.body)
     try {
-        const response = await axios.put(`http://localhost:5001/employees/${id}`, updateEmployee)
-        console.log(response)
+        const response = await axios.put(`${employeeServiceUrl}/employees/${id}`, updateEmployee)
+        console.log(response.data)
         return res.status(200).json({
             message: "funcionario atualizado com sucesso"
         })
@@ -70,11 +72,11 @@ async function updateEmployee(req, res) {
 async function deleteEmployee(req, res) {
     const { id } = req.params
     try {
-        const response = await axios.delete(`http://localhost:5001/employees/${id}`)
+        const response = await axios.delete(`${employeeServiceUrl}/employees/${id}`)
         const user = response.data
         if (user) {
             return res.status(200).json({
-                Employees: user
+                employees: user
             })
         }
     } catch (error) {
